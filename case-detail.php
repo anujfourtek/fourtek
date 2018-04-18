7cc35c1e93915856c99c19a595263079f1c62f1b
Ankit Singh <ankitsingh@fourtek.com>
2018-04-18 17:23:32 +0530

    github

---
+++ case-detail.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Greyorange</title>
+  
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+ <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+    <?php include('include/header.php');?>
+  <div class="clear-fix"></div>
+
+	<section class="inner-banner">
+	  <div class="container">
+	   <div class="inner-head">
+	       
+		   <nav aria-label="breadcrumb">
+		   <h1>Omni Channel</h1>
+			  <ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+				<li class="breadcrumb-item" aria-current="page"><a href="case.php">Case Studies</a></li>
+				<li class="breadcrumb-item active" aria-current="page">Omni Channel</li>
+			  </ol>
+			</nav>
+		  </div>
+		</div>
+		
+	    <div class="banner-image">
+   	     <img src="images/case-detail-banner.jpg" alt="case"/>
+		</div>
+		<div class="container">
+		
+		<div class="join">
+			<div class="col-md-12 col-sm-12">
+			  <div class="row">
+				 <div class="offset-7 col-sm-5 info-block">		 
+					  <div class="join-us">
+						 <p><strong>Consumer:</strong> MNC consumer goods leader in Gartner supply chain top 25 Improving efficiency of regional DCs (RDC)</p>
+					  </div> 
+				</div> 
+			  </div>
+			</div>
+        </div>
+		
+	  </div>
+	</section>
+
+
+ <section class="case-product-desc">
+    <div class="container">
+    
+   	  <h2>Omni - channel fulfillment center</h2>
+	  <p>Leading home improvement store chain in Latin America</p>
+	
+	<div class="case-detail-desc row">  
+	  <div class="case-issue col-md-6 col-sm-12">
+	     <ul class="list-group">
+		  <li class="list-group-item active case-problem">What is The Problem?</li>
+		  <li class="list-group-item">Manage complex omni-channel
+deliveries</li>
+		  <li class="list-group-item">Manage multiple inventory
+types (pallets, cases, inner
+cases, and eaches)
+Higher number of SKUs</li>
+		</ul>
+	  </div>
+	  <div class="case-solve col-md-6 col-sm-12">
+	     <ul class="list-group">
+		  <li class="list-group-item active case-solution">Solutions we provide?</li>
+		  <li class="list-group-item">Unified order picking and
+consolidation model
+Reduced dock to stock time
+and days on hand inventory
+at client's stores</li>
+		  <li class="list-group-item">Manpower savings and space
+optimizations</li>
+		</ul>
+	  </div>	  
+	  
+	  </div>  
+	</div>  
+ </section>
+ 
+ 
+ 
+
+ <?php include('include/footer.php');?>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+
+  <script src="js/main.js"></script>
+ 
+ 
+
+  <script>
+    $('document').ready(function(){
+	 $('#header').addClass('inner-header');
+     $('body').addClass('inner-page'); 
+	});	
+  </script>
+
+<script type="text/javascript">
+             $(document).ready(function () {
+                 $('#sidebarCollapse').on('click', function () {
+                     $('#sidebar').toggleClass('active');
+                 });
+             });
+         </script>
+
+  
+</body>
+</html>
