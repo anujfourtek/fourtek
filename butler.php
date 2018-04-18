116883af93d41ae7954c21e84b2dccb7528285d2
Ankit Singh <ankitsingh@fourtek.com>
2018-04-18 16:55:33 +0530

    github

---
+++ butler.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Greyorange</title>
+  
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+ <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+    <?php include('include/header.php');?>
+  <div class="clear-fix"></div>
+
+	<section class="inner-banner butler-bg">
+	  <div class="container">
+	   <div class="inner-head">
+	       
+		   <nav aria-label="breadcrumb">
+		   <h1>Butler</h1>
+			  <ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+				<li class="breadcrumb-item active" aria-current="page">Butler</li>
+			  </ol>
+			</nav>
+		</div>
+		</div>
+		<div class="banner-image">
+	     <img src="images/butler_banner.jpg" alt="butler"/>
+		</div>
+        <div class="container">		
+		    <div class="butler-short-desc"><br/>
+			  <p>Reducing order fulfillment, inventory replenishment time, 
+			  and ability to quickly adapt to changing business environments in warehouses is imperative for businesses today to gain 
+			  a competitive advantage. </p><br/>
+			</div> 
+		</div>
+		
+		 <div class="butler-intro">
+		  <div class="container">	
+		  <div class="row">
+		    <div class="col-md-6 col-sm-12">
+  		      <h3>Introducing GreyOrange Butler</h3>
+			  <p>An innovative goods-to-person technology, powered by Artificial Intelligence,
+			     for automated inventory management; storage (put away) and replenishment; picking and combining orders in
+			     fulfillment and distribution centers.</p>
+		    </div>
+			<div class="col-md-6 col-sm-12">
+			  <div class="embed-responsive embed-responsive-16by9"> 
+			  <iframe width="100%" height="200" src="https://www.youtube.com/embed/bnPn5qL1X5Q?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			  <!-- <video width="100%" height="215" controls>
+				  <source src="images/greyorange.mp4" type="video/mp4">
+				</video>-->
+			  </div>
+			</div>
+			
+	     </div>
+	    </div>
+	    </div>
+		 
+		 
+		 
+	  </div>
+	</section>
+
+
+ <section class="order-customer">
+    <div class="container">
+      <div class="row">
+	 
+	  <h2>Order Fulfillment Optimized</h2>
+	  <p>The Butler system consists of Butler Robots, Pick/Put stations, Mobile Storage Units (MSU), Auto-Charging stations, 
+	  and the brain of the system is the GreyMatter(TM), an end-to-end intelligent order fulfillment software platform that controls 
+	  and manages various devices to drive autonomous collaboration. </p>
+	
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/growth.png" alt="ENHANCES BUSINESS AGILITY AND GROWTH" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+			<h4>ENHANCES BUSINESS AGILITY AND GROWTH</h4>
+			<p>Adapts to changing inventory profiles, demand patterns and peaks</p>
+		  </div>					   
+        </div>
+    </div>
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/lower-cost.png" alt="LOWER OPERATING COSTS" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+	        <h4>LOWER OPERATING COSTS</h4>
+	        <p>Operator time saving by up to 70%</p>	 
+		  </div>					   
+        </div>
+    </div>
+	
+	
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/eliminate.png" alt="ELIMINATE COMPLEXITY" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+			 <h4>ELIMINATE COMPLEXITY</h4>
+			 <p>Reduces waste, increases efficiency </p>	 
+		  </div>					   
+        </div>
+    </div>
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/business.png" alt="BUSINESS CONTINUITY" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+	         <h4>BUSINESS CONTINUITY</h4>
+	        <p>24/7 operations and up to 99% availability</p>	
+		  </div>					   
+        </div>
+    </div>
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/empowerd-workforce.png" alt="EMPOWERED WORKFORCE" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+			 <h4>EMPOWERED WORKFORCE</h4>
+			 <p>Improves employee satisfaction</p>	
+		  </div>					   
+        </div>
+    </div>
+	
+	<div class="col-md-4 col-sm-12">
+	    <div class="row">
+	      <div class="col-md-3 col-sm-12 ico"><img src="images/speed_icons.png" alt="IMPROVES SPEED AND ACCURACY" class="img-fluid" /></div>
+		  <div class="col-md-9 col-sm-12">
+			 <h4>IMPROVES SPEED AND ACCURACY</h4>
+			 <p>Faster and accurate fulfillment</p>
+		  </div>					   
+        </div>
+    </div>
+ 
+     <div class="col-md-12 col-sm-12">
+	    <a href="javascript:;" class="btn cta-btn">Download Brochure </a>
+	    <a href="case.php" class="btn cta-btn">Check Case Studies</a>
+	 </div>
+	
+     </div>  
+	</div>  
+ </section>
+ 
+ <section class="workforce-block">
+	<div class="workforce">
+	  <div class="container">
+	 
+	   <h2>The Workforce</h2>
+		 <ul class="nav nav-tabs" role="tablist">
+		  <li class="nav-item">
+			<a class="nav-link active" data-toggle="tab" href="#butler" role="tab" aria-controls="butler">Butler</a>
+		  </li>
+		  <li class="nav-item">	
+			<a class="nav-link" data-toggle="tab" href="#MSU" role="tab" aria-controls="MSU">MSU</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" data-toggle="tab" href="#PPS" role="tab" aria-controls="PPS">PPS</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" data-toggle="tab" href="#charging" role="tab" aria-controls="charging">Charging</a>
+		  </li>
+		</ul>
+	   </div>
+	 </div>
+	 
+  <div class="container">	  
+	<div class="tab-content">
+	
+	<div class="tab-pane active" id="butler" role="tabpanel">
+	  <div class="butler-blocks">  
+	  
+	    <div class="bulter-article">
+		 <div class="butler-tab-img"><img src="images/butler-robot-m.jpg" alt="" class="img-fluid" /></div>
+		 <div class="butler-tab-desc">
+		   <h3>The Butler Robot: M</h3>
+			<ul>
+			   <li>Multi-floor operations</li>
+			   <li>Payload : 500 kgs</li>
+			</ul>
+		 </div>
+	    </div><!--bulter-article-->
+		
+	    <div class="bulter-article">
+		 <div class="butler-tab-img"><img src="images/butler-robot.jpg" alt="" class="img-fluid" /></div>
+		 <div class="butler-tab-desc">
+		 <h3>The Butler Robot: M</h3>
+			<ul>
+			   <li>Multi-floor operations</li>
+			   <li>Payload : 600 kgs</li>
+			   <li>Payload type : Pallets, Drums,Sacks, Cartons  </li>
+			</ul>
+		 </div>
+	    </div><!--bulter-article-->
+		
+	   </div><!--butler-blocks-->	   
+	  </div><!--#home-->
+	  
+	  
+	  
+	  <div class="tab-pane  butler-blocks" id="MSU" role="tabpanel">
+		  <div class="butler-blocks">  
+		  <div class="row">
+			 <div class="col-sm-12 col-md-5"><img src="images/msu.jpg" alt="" class="img-fluid"  /></div>
+			 <div class="col-sm-12 col-md-7 butler-tab-desc"><br/>
+			  <h3>Mobile Storage Units (MSU)</h3>
+				<ul>
+				   <li>Store big and small cases, indivisible items , pallets and much more. </li>
+				   <li>Ability to reconfigure when inventory characteristics change over time.</li>
+				   <li>Variants : Hangers for apparel, bins for auto - spare parts and pallet barrel drums for manufacturing. </li>
+				</ul>
+			 </div>
+			</div><!--row-->		
+			
+		   </div><!--butler-blocks-->
+	  </div>
+	  
+	  
+	  <div class="tab-pane" id="PPS" role="tabpanel">
+	      <div class="butler-blocks">  
+		  
+			<div class="row">
+			 <div class="col-sm-12 col-md-5"><img src="images/pps.jpg" alt="" class="img-fluid"  /></div>
+			 <div class="col-sm-12 col-md-7 butler-tab-desc"><br/>
+			  <h3>Pick Put Stations(PPS)</h3>
+				<ul>
+				   <li>Multipurpose workstation with multi-lingual user interface for guided pick, pathway, audit and returns operations.</li>
+				   <li>Enables operator to fulfill up to 48 orders simultaneously .</li>
+				   <li>Variants : Supports various configurations including pallet handling, put from to conveyor and roll cages.  </li>
+				</ul>
+			 </div>
+			</div><!--row-->
+			
+		   </div><!--butler-blocks-->
+	  
+	  </div>
+	  <div class="tab-pane" id="charging" role="tabpanel">
+	      <div class="butler-blocks">  
+		  
+			<div class="row">
+			 <div class="col-sm-12 col-md-5"><img src="images/smart-charging-station.jpg" alt="" class="img-fluid" /></div>
+			 <div class="col-sm-12 col-md-7 butler-tab-desc"><br/>
+			  <h3>Smart Charging station</h3>
+				<ul>
+				   <li>Intelligent charging algorithm that enables opportunity charging.</li>
+				   <li>A full charge of 30 minutes ensures up to 8 hours of operations, while a quick charge of 5 minutes ensures 5 hours of operations.</li>
+				   <li>Variants : Supports various configurations including pallet handling, put from to conveyor and roll cages.  </li>
+				</ul>
+			 </div>
+			</div><!--row-->
+			
+		   </div><!--butler-blocks-->
+	  
+	  </div>
+	</div><!--tab-content-->
+	
+	 <br/>
+	 <p class="text-center">Sync Move ensures higher butler productivity and reduces operator wait time .</p>
+	 <p class="text-center">Rotatable Lift ensures efficient storage and reduced aisle space thereby improving the overall Butler system throughput.</p>
+ 
+      <hr/>
+	 </div><!--container-->
+	 
+      <div class="butler-intro">
+	    <div class="container">
+		  <div class="row">
+		    <div class="col-md-6 col-sm-12">
+  		      <h3>Introducing GreyOrange Butler</h3>
+			  <p>An innovative goods-to-person technology, powered by Artificial Intelligence,
+			     for automated inventory management; storage (putaway) and replenishment; picking and combining orders in
+			     fulfillment and distribution centers.</p>
+		    </div>
+			<div class="col-md-6 col-sm-12">
+			  <div class="embed-responsive embed-responsive-16by9"> 
+			   <iframe width="100%" height="200" src="https://www.youtube.com/embed/bnPn5qL1X5Q?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			  
+			  </div>
+			</div>
+			
+	      </div>
+	     </div>
+	    </div>
+	
+	 
+	<div class="container bulter-img-pics">
+	<div class="row">
+		<div class="col-md-6 col-sm-12">
+		  <div class="row">
+		   <img src="images/grey-metter-img.jpg" class="img-fluid" alt="" />
+		   </div>
+		</div>
+		
+       <div class="col-md-6 col-sm-12">
+	     <div class="row">
+		   <img src="images/pickto-ship-img.jpg" class="img-fluid" alt="" />
+	  	 </div>
+		</div>
+	</div>	
+	</div>	
+
+</section>
+	
+
+ <?php include('include/footer.php');?>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+
+  <script src="js/main.js"></script>
+ 
+ 
+
+	<script>
+	 $('document').ready(function(){
+	   $('#header').addClass('inner-header');	   
+	   $('#sidebarCollapse').on('click', function () {
+		 $('#sidebar').toggleClass('active');
+	   });
+	 });
+	</script>
+
+  <script>
+  $(function () {
+    $('#myTab a:last').tab('show')
+  })
+</script>
+</body>
+</html>
